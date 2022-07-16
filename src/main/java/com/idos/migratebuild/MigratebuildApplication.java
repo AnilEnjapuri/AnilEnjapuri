7685a2f05940e788deb1da8b6c106e8007dc8bba
Srikanth Enjapuri <srikanthenjapuri@gmail.com>
2022-07-16 11:54:39 +0530

    intial commits

---
+++ src/main/java/com/idos/migratebuild/MigratebuildApplication.java
@@ -0,0 +1,13 @@
+package com.idos.migratebuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigratebuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigratebuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idos/migratebuild/MigratebuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idos.migratebuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigratebuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
